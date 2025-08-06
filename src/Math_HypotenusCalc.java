81e877307e11e261650e6a54821fa47fceced9b2
Vengata Krishnan R <vengatakrishnan.r@gmail.com>
2025-08-06 23:37:39 +0530

    Math Class: Calculation of Hypotenus

---
+++ src/Math_HypotenusCalc.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Math_HypotenusCalc {
+
+    public static void main(String[] arg){
+
+        Scanner scanner = new Scanner(System.in);
+
+        double a;
+        double b;
+
+        System.out.println("Enter the A side: ");
+        a = scanner.nextDouble();
+
+        System.out.println("Enter the B Side: ");
+        b = scanner.nextDouble();
+
+        double c;
+
+        c = Math.sqrt((Math.pow(a,2) + Math.pow(b,2)));
+
+        System.out.println("The value is "+c);
+        scanner.close();
+
+    }
+}
